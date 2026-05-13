f573231662989d5a9bb8a719c44776cac7417b8c
Ivan Shcheklein <shcheklein@gmail.com>
2026-05-13 13:36:59 -0700

    remove redundant test

--- dvc_ssh/tests/test_client.py
+++ dvc_ssh/tests/test_client.py
@@ -8,22 +8,6 @@ from dvc_ssh.client import InteractiveSSHClient
 from dvc_ssh.tests.cloud import TEST_SSH_USER
 
 
-@pytest.mark.parametrize(
-    "password,expected",
-    [("secret", NotImplemented), (None, "")],
-)
-def test_kbdint_auth_requested(password, expected):
-    client = InteractiveSSHClient()
-    client._conn = SimpleNamespace(_options=SimpleNamespace(password=password))
-
-    result = client.kbdint_auth_requested()
-
-    if expected is NotImplemented:
-        assert result is NotImplemented
-    else:
-        assert result == expected
-
-
 def test_password_auth_uses_configured_password(ssh_server, monkeypatch, tmp_path):
     monkeypatch.setenv("HOME", str(tmp_path))
     monkeypatch.delenv("SSH_AUTH_SOCK", raising=False)
